ef88b01b40f1bbca70d85cb4a5038b8c23e6dfe7
bradley-hmcts <61189290+bradley-hmcts@users.noreply.github.com>
2022-01-18 17:16:42 +0000

    Revert "LAU-224 Increase session timeout. Implement IdAM token refresh. (#5)"
    This reverts commit 32228a69456584c9bad93159d5b9e7bcba8cfdda.

--- src/main/modules/oidc/index.ts
+++ src/main/modules/oidc/index.ts
@@ -1,7 +1,28 @@
-import {AuthService, IdamGrantType} from '../../service/AuthService';
+const logger = (require('@hmcts/nodejs-logging')).Logger.getLogger('oidc');
+
 import {Application, NextFunction, Response} from 'express';
+import fetch, {Response as FetchResponse} from 'node-fetch';
 import config from 'config';
+import jwt_decode from 'jwt-decode';
 import {AppRequest} from '../../models/appRequest';
+import {HttpResponseError} from '../../util/HttpResponseError';
+
+interface IdTokenJwtPayload {
+  uid: string;
+  sub: string;
+  given_name: string;
+  family_name: string;
+  roles: string[];
+}
+
+interface IdamResponseData {
+  access_token: string;
+  refresh_token: string;
+  scope: string;
+  id_token: string;
+  token_type: string;
+  expires_in: number;
+}
 
 /**
  * Adds the oidc middleware to add oauth authentication
@@ -17,11 +38,11 @@ export class OidcMiddleware {
     '/assets/images/favicon.ico',
   ];
 
-  private authService = new AuthService();
-
   public enableFor(server: Application): void {
     const loginUrl: string = config.get('services.idam.authorizationURL');
+    const tokenUrl: string = config.get('services.idam.tokenURL');
     const clientId: string = config.get('services.idam.clientID');
+    const clientSecret: string = config.get('services.idam.clientSecret');
     const redirectUri: string = config.get('services.idam.callbackURL');
 
     server.get('/login', (req: AppRequest, res) => {
@@ -29,12 +50,56 @@ export class OidcMiddleware {
     });
 
     server.get('/oauth2/callback', async (req: AppRequest, res: Response) => {
-      this.authService.getIdAMToken(IdamGrantType.AUTH_CODE, req.session, req.query.code as string)
-        .then(() => res.redirect('/'))
-        .catch(() => res.redirect('/'));
+
+      const params = new URLSearchParams({
+        client_id: clientId,
+        client_secret: clientSecret,
+        grant_type: 'authorization_code',
+        redirect_uri: redirectUri,
+        code: req.query.code as string,
+      });
+
+      let response: FetchResponse;
+      try {
+        response = await fetch(
+          tokenUrl,
+          {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: params.toString(),
+          },
+        );
+      } catch (e) {
+        logger.error(e);
+        return res.redirect('/');
+      }
+
+      try {
+        OidcMiddleware.checkStatus(response);
+      } catch (error) {
+        logger.error(error);
+
+        const errorBody = await error.response.text();
+        logger.error(`Error body: ${errorBody}`);
+        return res.redirect('/');
+      }
+
+      const data: IdamResponseData = await response.json();
+      const jwt: IdTokenJwtPayload = jwt_decode(data.id_token);
+
+      req.session.user = {
+        accessToken: data.access_token,
+        idToken: data.id_token,
+        id: jwt.uid,
+        roles: jwt.roles,
+      };
+      req.session.save(() => res.redirect('/'));
     });
 
-    server.get('/logout', (req: AppRequest, res) => {
+    server.get('/logout', function (req: AppRequest, res) {
       req.session.user = undefined;
       req.session.save(() => res.redirect('/'));
     });
@@ -61,6 +126,15 @@ export class OidcMiddleware {
     const css = /\/main\.([a-zA-Z]*[0-9]*[a-zA-Z]*)+\.css/gm;
 
     return js.test(path) || css.test(path);
+  }
+
+  private static checkStatus(response: FetchResponse): FetchResponse {
+    if (response.ok) {
+      // response.status >= 200 && response.status < 300
+      return response;
+    } else {
+      throw new HttpResponseError(response);
+    }
   }
 
 }
--- src/test/unit/service/CaseService.test.ts
+++ src/test/unit/service/CaseService.test.ts
@@ -1,12 +1,10 @@
 import nock from 'nock';
 import config from 'config';
-import sinon from 'sinon';
 import {CaseService} from '../../../main/service/CaseService';
 import {CaseActivityAudit} from '../../../main/models/case/CaseActivityAudit';
 import {CaseSearchRequest} from '../../../main/models/case/CaseSearchRequest';
 import {CaseSearchAudit} from '../../../main/models/case/CaseSearchAudit';
 import {AppRequest} from '../../../main/models/appRequest';
-import {AuthService} from '../../../main/service/AuthService';
 
 describe('Case Service', () => {
   const caseService = new CaseService();
@@ -45,47 +43,6 @@ describe('Case Service', () => {
       const caseActivities: CaseActivityAudit = await caseService.getCaseActivities(req as AppRequest);
 
       expect(caseActivities).toStrictEqual(caseActivityAudit);
-    });
-
-    it('refreshes IdAM session if expired', async () => {
-      const authService = new AuthService();
-      const sinonSpy = sinon.spy();
-      const spy = jest.spyOn(authService, 'getIdAMToken').mockImplementation(sinonSpy);
-      const spyCaseService = new CaseService(authService);
-
-      const caseActivityAudit: CaseActivityAudit = {
-        actionLog: [],
-        totalNumberOfRecords: 0,
-        startRecordNumber: 1,
-        moreRecords: false,
-      };
-
-      nock(baseApiUrl)
-        .get(`${caseActivitiesEndpoint}?userId=123&startTimestamp=2021-12-12T12:00:00&endTimestamp=2021-12-12T12:00:01`)
-        .reply(
-          200,
-          caseActivityAudit,
-        );
-
-      const searchParameters: Partial<CaseSearchRequest> = {
-        userId: '123',
-        startTimestamp: '2021-12-12T12:00:00',
-        endTimestamp: '2021-12-12T12:00:01',
-      };
-
-      const req = {
-        session: {
-          caseFormState: searchParameters,
-          user: {
-            expiresAt: Date.now(), // Date.now() ensures the expired statement evaluates as true
-          },
-        },
-      };
-
-      await spyCaseService.getCaseActivities(req as AppRequest);
-
-      expect(sinonSpy.called).toBeTruthy();
-      spy.mockClear();
     });
   });
 
