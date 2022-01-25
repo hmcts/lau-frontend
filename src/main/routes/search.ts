bbe34b0a37e2eaa5e89b788bbe5bfd36eeb31ae6
Bradley Wadsworth <bradley.wadsworth@hmcts.net>
2022-01-25 18:04:56 +0000

    Remove redundent search routes

--- src/main/routes/search.ts
+++ src/main/routes/search.ts
@@ -5,13 +5,4 @@ import {LogonSearchController} from '../controllers/LogonSearch.controller';
 export default function (app: Application): void {
   app.post('/case-search', (new CaseSearchController().post));
   app.post('/logon-search', (new LogonSearchController().post));
-
-  // Attempt to rectify issues with Demo environment redirecting with a GET call
-  app.get('/case-search', (req, res) => {
-    res.redirect('/#case-activity-tab');
-  });
-
-  app.get('/logon-search', (req, res) => {
-    res.redirect('/#logons-tab');
-  });
 }
