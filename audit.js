7c82ea70b7af3938f8dc7ff51eea0eb00c8e7a24
Dave Jones <dave.jones@hmcts.net>
2022-02-02 12:07:31 +0000

    LAU-256: Testing nightly build.

---
+++ audit.js
@@ -0,0 +1,8 @@
+{
+  "10104_User Agent Fuzzer_https://lau.aat.platform.hmcts.net/_GET": "ignore",
+  "10104_User Agent Fuzzer_https://lau.aat.platform.hmcts.net/login_GET": "ignore",
+  "10104_User Agent Fuzzer_https://lau.aat.platform.hmcts.net/robots.txt_GET": "ignore",
+  "10104_User Agent Fuzzer_https://lau.aat.platform.hmcts.net/robots.txt_GET": "ignore",
+  "10104_User Agent Fuzzer_https://lau.aat.platform.hmcts.net/sitemap.xml_GET": "ignore",
+  "10104_User Agent Fuzzer_https://lau.aat.platform.hmcts.net/sitemap.xml_GET": "ignore"
+}
