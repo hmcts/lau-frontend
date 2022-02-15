0a849d6b9649e399ead6ab07a0bf9248e68daaf3
Bradley Wadsworth <bradley.wadsworth@hmcts.net>
2022-02-15 13:39:39 +0000

    LAU-262 Add Util unit tests

--- src/main/util/Util.ts
+++ src/main/util/Util.ts
@@ -1,11 +1,3 @@
-export const formatQueryString = function (params: Record<string, unknown>): string {
-  const qs = Object.keys(params)
-    .map(key => key + '=' + params[key])
-    .join('&');
-
-  return `/service-endpoint?${qs}`;
-};
-
 export const numberWithCommas = function (x: string | number): string {
   const num: number = typeof x === 'string' ? parseInt(x) : x;
   return isNaN(num) ? '' : num.toLocaleString();
