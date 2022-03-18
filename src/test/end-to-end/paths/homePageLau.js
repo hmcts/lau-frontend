500fc99ca72c7beecacc4cdc653204aef69a26c6
Bradley Wadsworth <bradley.wadsworth@hmcts.net>
2022-03-18 14:40:36 +0000

    LAU-339 Add helper to assert CSV files

--- src/test/end-to-end/paths/homePageLau.js
+++ src/test/end-to-end/paths/homePageLau.js
@@ -38,17 +38,20 @@ Scenario('Navigate to LAU, perform case audit search and authenticate case activ
   //Asserting the CSV after hitting download button
   const textBeforePagination = await I.grabTextFromAll('div[class="flex-space-between"] p');
   logger.info({message: 'the text is ', textBeforePagination});
-  // await I.waitForText('Displaying 1 to 100 of 10,000 records, Note: Results returned have been capped at 10,000.');
+  await I.waitForText('Displaying 1 to 100 of 10,000 records');
+  await I.waitForText('Note: Results returned have been capped at 10,000.');
   await I.click('Next >');
   await I.wait(10);
   const textAfterPagination = await I.grabTextFromAll('div[class="flex-space-between"] p');
   logger.info({message: 'the text is ', textAfterPagination});
-  // await I.waitForText('Displaying 2 to 100 of 10,000 records, Note: Results returned have been capped at 10,000.');
+  await I.waitForText('Displaying 101 to 200 of 10,000 records');
+  await I.waitForText('Note: Results returned have been capped at 10,000.');
   await I.click('Last >>');
   await I.wait(10);
   const textLastPagination = await I.grabTextFromAll('div[class="flex-space-between"] p');
   logger.info({message: 'the text is ', textLastPagination});
-  // await I.waitForText('Displaying 100 to 100 of 10,000 records, Note: Results returned have been capped at 10,000.');
+  await I.waitForText('Displaying 9,901 to 10,000 of 10,000 records');
+  await I.waitForText('Note: Results returned have been capped at 10,000.');
 
 }).retry(testConfig.TestRetryScenarios);
 
@@ -72,4 +75,25 @@ Scenario('Navigate to LAU, perform case audit search and authenticate case searc
 
 }).retry(testConfig.TestRetryScenarios);
 
+Scenario('Navigate to LAU, perform case audit search and download CSV', async ({I}) => {
+  await I.amOnLauAppPage('');
+  await I.authenticateWithIdam(userType.AUDITOR, true);
 
+  await I.amOnPage('/');
+  await I.waitForText('Log and Audit');
+  await I.waitForText('Case Audit Search', testConfig.TestTimeToWaitForText);
+  await I.performCaseAuditSearch();
+  await I.click('//button[@name="case-search-btn"]');
+  await I.wait(5);
+  await I.waitForText('Case Activity Results');
+  await I.handleDownloads();
+  await I.click('#activityCsvBtn');
+  await I.wait(10);
+
+  const csvPath = lauHelper.getCsvPath();
+
+  await I.amInPath(csvPath.codeceptPath);
+  await I.seeFile(csvPath.filename);
+  lauHelper.assertCsvLineCount(csvPath.fullPath, 10000);
+
+}).retry(testConfig.TestRetryScenarios);
