85d8966e40d7e77153bb96f4a1147b52862453e3
bradley-hmcts <61189290+bradley-hmcts@users.noreply.github.com>
2022-05-09 10:23:01 +0100

    LAU-376 Upgrade Cookie Manager V1 (under toggle) (#57)

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,7 +2,12 @@
 import { initAll } from 'govuk-frontend';
 
 import './assets/scss/main.scss';
-import './assets/js/cookie';
+
+import {initCookieManager} from './assets/js/cookie';
+import {initCookieManagerV1} from './assets/js/cookie-v1';
+const useCookieManagerV1 = document.getElementById('use-cookie-manager-v1').textContent === 'true';
+useCookieManagerV1 ? initCookieManagerV1() : initCookieManager();
+
 import './assets/js/flatpickr';
 import './assets/js/home';
 
