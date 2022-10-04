6927b79241bfba54dc2583f0420987966e1d296b
bradley-hmcts <61189290+bradley-hmcts@users.noreply.github.com>
2022-10-04 11:21:08 +0100

    LAU-438 Deletion logging (#69)
    * LAU-438 Deletion logging
    
    * LAU-438 Remove deletion mode and add case ref and type to deletions
    
    * LAU-438 Fix minor refactoring errors
    
    * LAU-438 Increase timeout for logon audit results
    
    * LAU-438 Increase wait for text timeouts after search
    
    * LAU-438 Increase wait times after searching
    
    * LAU-438 Update for backend integration
    
    * LAU-438 Point case backend to PR
    
    * LAU-438 Fix backend PR url
    
    * LAU-438 Move backend PR url config
    
    * Bumping chart version/ fixing aliases
    
    * LAU-438 Remove case-backend frm healthcehck temporarily
    
    * LAU-438 Add logging
    
    * LAU-438 Logging
    
    * LAU-438 Set case-backend PR url to https
    
    * LAU-438 Revert backend url to http
    
    * LAU-438: Update to correct backend URL
    
    * LAU-438: Reverting back to AAT case backend.
    
    Co-authored-by: hmcts-jenkins-j-to-z <61242337+hmcts-jenkins-j-to-z[bot]@users.noreply.github.com>
    Co-authored-by: Dave Jones <38425925+davejones74@users.noreply.github.com>
    Co-authored-by: Dave Jones <dave.jones@hmcts.net>

--- src/main/service/CaseService.ts
+++ src/main/service/CaseService.ts
@@ -3,11 +3,13 @@ import {BaseService} from './BaseService';
 import config from 'config';
 import {CaseActivityAudit} from '../models/case/CaseActivityAudit';
 import {CaseSearchRequest} from '../models/case/CaseSearchRequest';
+import {CaseDeletionsSearchRequest} from '../models/deletions/CaseDeletionsSearchRequest';
 import {CaseSearchAudit} from '../models/case/CaseSearchAudit';
 import {AppRequest} from '../models/appRequest';
 import {ErrorCode} from '../models/AppError';
+import {CaseDeletions} from '../models/deletions/CaseDeletions';
 
-export class CaseService extends BaseService<CaseSearchRequest> {
+export class CaseService extends BaseService<CaseSearchRequest | CaseDeletionsSearchRequest> {
   baseApiUrl = String(config.get('services.lau-case-backend.url'));
   errorCode = ErrorCode.CASE_BACKEND;
 
@@ -19,6 +21,7 @@ export class CaseService extends BaseService<CaseSearchRequest> {
       searchParameters.page = 1;
       searchParameters.size = req.session.caseActivities?.totalNumberOfRecords || 0;
     }
+
     this.logger.info('getCaseActivities: ' + JSON.stringify(searchParameters) + ' CSV: ' + csv);
     return this.get(req.session, endpoint, this.getQueryString(searchParameters)) as Promise<CaseActivityAudit>;
   }
@@ -33,5 +36,31 @@ export class CaseService extends BaseService<CaseSearchRequest> {
     }
     this.logger.info('getCaseSearches: ' + JSON.stringify(searchParameters) + ' CSV: ' + csv);
     return this.get(req.session, endpoint, this.getQueryString(searchParameters)) as Promise<CaseSearchAudit>;
+  }
+
+  public getCaseDeletions(req: AppRequest, csv = false): Promise<CaseDeletions> {
+    const endpoint: string = config.get('services.lau-case-backend.endpoints.caseActivity');
+    // Shallow clone state to prevent modifications to search params for csv case do not persist in session
+    const searchParameters = Object.assign({}, req.session.caseDeletionsFormState) || {};
+    if (csv) {
+      searchParameters.page = 1;
+      searchParameters.size = req.session.caseDeletions?.totalNumberOfRecords || 0;
+    }
+    this.logger.info('getCaseDeletions: ' + JSON.stringify(searchParameters) + ' CSV: ' + csv);
+
+    return this.get(req.session, endpoint, this.getQueryString(searchParameters)).then((caseActivityAudit: CaseActivityAudit) => {
+      const actionLog = caseActivityAudit.actionLog ? caseActivityAudit.actionLog.map(log => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { userId, caseAction, ...deletionsLog } = log;
+        return deletionsLog;
+      }) : null;
+
+      const caseDeletions: CaseDeletions = {
+        ...caseActivityAudit,
+        actionLog,
+      };
+
+      return caseDeletions;
+    });
   }
 }
---
+++ src/test/unit/controllers/case-deletions-search.test.ts
@@ -0,0 +1,170 @@
+import nock from 'nock';
+import sinon from 'sinon';
+import {AppRequest} from '../../../main/models/appRequest';
+import {CaseDeletionsSearchController} from '../../../main/controllers/CaseDeletionsSearch.controller';
+
+describe('Case Deletions Search Controller', () => {
+  describe('Search form validation', () => {
+    const caseDeletionsSearchController: CaseDeletionsSearchController = new CaseDeletionsSearchController();
+
+    it('requires at least one of the string inputs', async () => {
+      const errors = caseDeletionsSearchController.validateSearchForm({
+        caseRef: '',
+        caseTypeId: '',
+        caseJurisdictionId: '',
+        startTimestamp: '2021-01-01 00:00:00',
+        endTimestamp: '2021-01-01 00:00:01',
+        page: 1,
+      });
+      expect(errors.length).toBe(1);
+      expect(errors[0]).toStrictEqual({propertyName: 'caseDeletionsSearchForm', errorType: 'stringFieldRequired'});
+    });
+
+    it('requires both date inputs to be filled', async () => {
+      let errors = caseDeletionsSearchController.validateSearchForm({
+        startTimestamp: '',
+        endTimestamp: '',
+      });
+      expect(errors.length).toBe(2);
+      expect(errors[0].errorType).toBe('required');
+      expect(errors[1].errorType).toBe('required');
+
+      errors = caseDeletionsSearchController.validateSearchForm({
+        startTimestamp: '2021-01-01 00:00:00',
+        endTimestamp: '',
+      });
+      expect(errors.length).toBe(1);
+      expect(errors[0]).toStrictEqual({propertyName: 'endTimestamp', errorType: 'required'});
+
+      errors = caseDeletionsSearchController.validateSearchForm({
+        startTimestamp: '',
+        endTimestamp: '2021-01-01 00:00:00',
+      });
+      expect(errors.length).toBe(1);
+      expect(errors[0]).toStrictEqual({propertyName: 'startTimestamp', errorType: 'required'});
+    });
+
+    it('ensures dates are formatted correctly', async () => {
+      const errors = caseDeletionsSearchController.validateSearchForm({
+        startTimestamp: '2021-01-01T00:00:00',
+        endTimestamp: '2021-01-01 00:00:00',
+      });
+      expect(errors.length).toBe(1);
+      expect(errors[0]).toStrictEqual({propertyName: 'startTimestamp', errorType: 'invalid'});
+    });
+
+    it('ensures dates are valid', async () => {
+      const errors = caseDeletionsSearchController.validateSearchForm({
+        startTimestamp: '2021-14-01 00:00:00',
+        endTimestamp: '2021-12-01 00:00:00',
+      });
+      expect(errors.length).toBe(1);
+      expect(errors[0]).toStrictEqual({propertyName: 'startTimestamp', errorType: 'invalid'});
+    });
+
+    it('passes valid dates', async () => {
+      const errors = caseDeletionsSearchController.validateSearchForm({
+        startTimestamp: '2021-01-01 00:00:00',
+        endTimestamp: '2021-01-01 00:00:01',
+      });
+      expect(errors.length).toBe(0);
+    });
+  });
+
+  describe('Post call', () => {
+    const caseDeletionsSearchController: CaseDeletionsSearchController = new CaseDeletionsSearchController();
+
+    const res = {
+      redirect: sinon.spy(),
+    };
+
+    beforeEach(() => {
+      nock.cleanAll();
+      res.redirect.resetHistory();
+    });
+
+    it('formats the search request', async () => {
+      nock('http://localhost:4550')
+        .get('/audit/caseAction?caseRef=123&caseTypeId=123&caseJurisdictionId=123&startTimestamp=2021-12-12T12:00:00&endTimestamp=2021-12-12T12:00:01&page=1&caseAction=DELETE&size=5')
+        .reply(
+          200,
+          {actionLog: []},
+        );
+
+      const req = {
+        session: {},
+        body: {
+          caseRef: '123',
+          caseTypeId: '123',
+          caseJurisdictionId: '123',
+          startTimestamp: '2021-12-12 12:00:00',
+          endTimestamp: '2021-12-12 12:00:01',
+          page: 1,
+        },
+      };
+
+      // @ts-ignore
+      return caseDeletionsSearchController.post(req as AppRequest, res as Response).then(() => {
+        expect(req.body).toStrictEqual({
+          caseAction: 'DELETE',
+          caseRef: '123',
+          caseTypeId: '123',
+          caseJurisdictionId: '123',
+          startTimestamp: '2021-12-12T12:00:00',
+          endTimestamp: '2021-12-12T12:00:01',
+          page: 1,
+          size: 5,
+        });
+      });
+    });
+
+    it('redirects to the case deletions results tab', async () => {
+      nock('http://localhost:4550')
+        .get('/audit/caseAction?caseRef=123&caseTypeId=123&caseJurisdictionId=123&startTimestamp=2021-12-12T12:00:00&endTimestamp=2021-12-12T12:00:01&caseAction=DELETE&size=5')
+        .reply(
+          200,
+          {actionLog: []},
+        );
+
+      const req = {
+        session: {},
+        body: {
+          caseRef: '123',
+          caseTypeId: '123',
+          caseJurisdictionId: '123',
+          startTimestamp: '2021-12-12 12:00:00',
+          endTimestamp: '2021-12-12 12:00:01',
+        },
+      };
+
+      // @ts-ignore
+      return caseDeletionsSearchController.post(req as AppRequest, res as Response).then(() => {
+        expect(res.redirect.calledOnce).toBeTruthy();
+        expect(res.redirect.calledWith('/#case-deletions-tab')).toBeTruthy();
+      });
+    });
+
+    it('redirects to error page with backend error code', async () => {
+      nock('http://localhost:4550')
+        .get('/audit/caseAction?caseRef=123&caseTypeId=123&caseJurisdictionId=123&startTimestamp=2021-12-12T12:00:00&endTimestamp=2021-12-12T12:00:01&caseAction=DELETE&size=5')
+        .reply(500, {});
+
+      const req = {
+        session: {},
+        body: {
+          caseRef: '123',
+          caseTypeId: '123',
+          caseJurisdictionId: '123',
+          startTimestamp: '2021-12-12 12:00:00',
+          endTimestamp: '2021-12-12 12:00:01',
+        },
+      };
+
+      // @ts-ignore
+      return caseDeletionsSearchController.post(req as AppRequest, res as Response).then(() => {
+        expect(res.redirect.calledOnce).toBeTruthy();
+        expect(res.redirect.calledWith('/error?code=LAU02')).toBeTruthy();
+      });
+    });
+  });
+});
