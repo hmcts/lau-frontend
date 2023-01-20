721e8bd09c668cbdfeabf9fde199f569dedbed40
Bradley Wadsworth <bradley.wadsworth@hmcts.net>
2023-01-20 15:47:35 +0000

    LAU-594 Add login state column

--- src/main/models/idam/LogonLogs.ts
+++ src/main/models/idam/LogonLogs.ts
@@ -4,10 +4,11 @@ export interface LogonLog {
   userId: string;
   emailAddress: string;
   service: string;
+  loginState: string;
   ipAddress: string;
   timestamp: string;
 }
 
 export class LogonLogs extends Logs<LogonLog> {
-  public _fields: string[] = ['userId', 'emailAddress', 'service', 'ipAddress', 'timestamp'];
+  public _fields: string[] = ['userId', 'emailAddress', 'service', 'loginState', 'ipAddress', 'timestamp'];
 }
