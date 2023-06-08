f07462657bb213ea6a6add85fccd770bdef99021
Martin <martin.spasov@hmcts.net>
2023-06-08 12:00:03 +0100

    LAU-670 Upgrade Redis cache to v6

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -15,7 +15,7 @@ data "azurerm_subnet" "core_infra_redis_subnet" {
 
 
 module "lau-frontend-session-storage" {
-  source                          = "git@github.com:hmcts/cnp-module-redis?ref=master"
+  source                          = "git@github.com:hmcts/cnp-module-redis?ref=add-redis-version-flag"
   product                         = "${var.product}-${var.component}-session-storage"
   location                        = var.location
   env                             = var.env
