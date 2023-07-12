a30074491b0aefc24fbe40d1f6e359f9e42edf8b
Martin <martin.spasov@hmcts.net>
2023-07-12 12:43:41 +0100

    Unable debugging in preview

--- src/main/modules/appinsights/index.ts
+++ src/main/modules/appinsights/index.ts
@@ -1,10 +1,22 @@
+import {LoggerInstance} from 'winston';
+const {Logger} = require('@hmcts/nodejs-logging');
+
 import config from 'config';
+import Sender from 'applicationinsights/out/Library/Sender';
+import * as os from 'os';
+import path from 'path';
+import fs from 'fs';
 
 const appInsights = require('applicationinsights');
 
 export class AppInsights {
+
+  private logger: LoggerInstance = Logger.getLogger(this.constructor.name);
+
   enable(): void {
     if (config.get('appInsights.instrumentationKey')) {
+      this.createTempDir();
+
       appInsights
         .setup(config.get('appInsights.instrumentationKey'))
         .setSendLiveMetrics(true)
@@ -20,4 +32,14 @@ export class AppInsights {
       appInsights.defaultClient.trackTrace({message: 'App insights activated'});
     }
   }
+
+  private createTempDir(): void {
+    const tempDir = path.join(os.tmpdir(), Sender.TEMPDIR_PREFIX + config.get('appInsights.instrumentationKey'));
+
+    if (!fs.existsSync(tempDir)) {
+      this.logger.info('Creating App Insights temp dir');
+      fs.mkdirSync(tempDir);
+    }
+  }
+
 }
