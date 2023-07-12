9837cda8f3bfa3e0b74727a5f01ef02cc5e2deb7
Martin <martin.spasov@hmcts.net>
2023-07-12 11:54:33 +0100

    Unable debugging in preview

--- src/main/modules/appinsights/index.ts
+++ src/main/modules/appinsights/index.ts
@@ -1,41 +1,23 @@
-import {LoggerInstance} from 'winston';
-const {Logger} = require('@hmcts/nodejs-logging');
-
 import config from 'config';
-import Sender from 'applicationinsights/out/Library/Sender';
-import * as os from 'os';
-import path from 'path';
-import fs from 'fs';
 
 const appInsights = require('applicationinsights');
 
 export class AppInsights {
-
-  private logger: LoggerInstance = Logger.getLogger(this.constructor.name);
-
   enable(): void {
     if (config.get('appInsights.instrumentationKey')) {
-      this.createTempDir();
-
-      this.logger.info('Starting App Insights');
-
-      appInsights.setup(config.get<string>('appInsights.instrumentationKey'))
+      appInsights
+        .setup(config.get('appInsights.instrumentationKey'))
         .setSendLiveMetrics(true)
         .setAutoCollectConsole(true, true)
+        .setAutoCollectExceptions(true)
         .start();
 
-      appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = 'lau-frontend';
+      appInsights.defaultClient.addTelemetryProcessor(
+        (env, ctx) =>
+          ctx['http.ServerResponse']?.req.url !== '/csrf-token-error' && ctx['http.ServerResponse']?.statusCode !== 404,
+      );
+      appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = 'nfdiv-frontend';
       appInsights.defaultClient.trackTrace({message: 'App insights activated'});
     }
   }
-
-  private createTempDir(): void {
-    const tempDir = path.join(os.tmpdir(), Sender.TEMPDIR_PREFIX + config.get('appInsights.instrumentationKey'));
-
-    if (!fs.existsSync(tempDir)) {
-      this.logger.info('Creating App Insights temp dir');
-      fs.mkdirSync(tempDir);
-    }
-  }
-
 }
