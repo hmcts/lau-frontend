b27d1f7dae29e910ac9ead8bef86720a9f23a67a
Martin <martin.spasov@hmcts.net>
2023-07-14 14:42:27 +0100

    Tidy up some code

--- src/test/end-to-end/paths/homePageLau.js
+++ src/test/end-to-end/paths/homePageLau.js
@@ -32,7 +32,6 @@ Scenario('Navigate to LAU, perform case audit search and authenticate case activ
   await I.waitForText('Case Audit Search', testConfig.TestTimeToWaitForText);
   await I.performCaseAuditSearch();
   await I.click('button[name="case-search-btn"]');
-  await I.wait(10);
   await I.waitForText('Case Activity Results', testConfig.TestTimeToWaitForText);
   // Asserting the text after Pagination
   const textBeforePagination = await I.grabTextFromAll('div[class="flex-space-between"] p');
@@ -40,13 +39,11 @@ Scenario('Navigate to LAU, perform case audit search and authenticate case activ
   await I.waitForText('Displaying 1 to 100 of 10,000 records', testConfig.TestTimeToWaitForText);
   await I.waitForText('Note: Results returned have been capped at 10,000.', testConfig.TestTimeToWaitForText);
   await I.click('Next >');
-  await I.wait(10);
   const textAfterPagination = await I.grabTextFromAll('div[class="flex-space-between"] p');
   logger.info({message: 'the text is ', textAfterPagination});
   await I.waitForText('Displaying 101 to 200 of 10,000 records', testConfig.TestTimeToWaitForText);
   await I.waitForText('Note: Results returned have been capped at 10,000.', testConfig.TestTimeToWaitForText);
   await I.click('Last >>');
-  await I.wait(10);
   const textLastPagination = await I.grabTextFromAll('div[class="flex-space-between"] p');
   logger.info({message: 'the text is ', textLastPagination});
   await I.waitForText('Displaying 9,901 to 10,000 of 10,000 records', testConfig.TestTimeToWaitForText);
@@ -63,7 +60,6 @@ Scenario('Navigate to LAU, perform case audit search and authenticate case searc
   await I.waitForText('Case Audit Search', testConfig.TestTimeToWaitForText);
   await I.performCaseSearch();
   await I.click('button[name="case-search-btn"]');
-  await I.wait(10);
   await lauHelper.selectTab(I, tabs.CASE_SEARCHES);
   // await I.waitForText('Case Search Results', testConfig.TestTimeToWaitForText);
   // Asserting the text after Pagination
@@ -71,12 +67,10 @@ Scenario('Navigate to LAU, perform case audit search and authenticate case searc
   logger.info({message: 'the text is ', textBeforePaginationOfCaseSearch});
   await I.waitForText('Displaying 1 to 100 of 132 records', testConfig.TestTimeToWaitForText);
   await I.click('Next >');
-  await I.wait(10);
   const textAfterPaginationOfCaseSearch = await I.grabTextFromAll('div[class="flex-space-between"] p');
   logger.info({message: 'the text is ', textAfterPaginationOfCaseSearch});
   await I.waitForText('Displaying 101 to 132 of 132 records', testConfig.TestTimeToWaitForText);
   await I.click('< Previous');
-  await I.wait(10);
   const textPreviousPageOfCaseSearch = await I.grabTextFromAll('div[class="flex-space-between"] p');
   logger.info({message: 'the text is ', textPreviousPageOfCaseSearch});
   await I.waitForText('Displaying 1 to 100 of 132 records', testConfig.TestTimeToWaitForText);
@@ -93,11 +87,9 @@ Scenario('Navigate to LAU, perform case audit search and download CSV', async ({
   await I.waitForText('Case Audit Search', testConfig.TestTimeToWaitForText);
   await I.performCaseAuditSearch();
   await I.click('button[name="case-search-btn"]');
-  await I.wait(10);
   await I.waitForText('Case Activity Results', testConfig.TestTimeToWaitForText);
   await I.handleDownloads();
   await I.click('#activityCsvBtn');
-  await I.wait(10);
 
   const csvPath = lauHelper.getCsvPath();
 
@@ -116,12 +108,10 @@ Scenario('Navigate to LAU, perform case search and download CSV', async ({I}) =>
   await I.waitForText('Case Audit Search', testConfig.TestTimeToWaitForText);
   await I.performCaseSearch();
   await I.click('button[name="case-search-btn"]');
-  await I.wait(10);
   await lauHelper.selectTab(I, tabs.CASE_SEARCHES);
   // await I.waitForText('Case Search Results', testConfig.TestTimeToWaitForText);
   await I.handleDownloads();
   await I.click('#searchesCsvBtn');
-  await I.wait(10);
 
   const csvPath = lauHelper.getCsvPath();
 
@@ -141,7 +131,6 @@ Scenario('Navigate to LAU, perform case audit search and authenticate error text
   await I.waitForText('Case Audit Search', testConfig.TestTimeToWaitForText);
   await I.performCaseAuditSearchWithoutSearchData();
   await I.click('button[name="case-search-btn"]');
-  await I.wait(30);
   await I.waitForText('Please enter at least one of the following fields: User ID, Case Type ID, Case Ref or Jurisdiction ID.', testConfig.TestTimeToWaitForText);
   await I.waitForText('\'Time from\' is required.', testConfig.TestTimeToWaitForText);
   await I.waitForText('\'Time to\' is required.', testConfig.TestTimeToWaitForText);
--- src/test/end-to-end/paths/logonPage.js
+++ src/test/end-to-end/paths/logonPage.js
@@ -18,7 +18,6 @@ Scenario('Navigate to LAU, perform logon audit search and authenticate logon sea
   await I.waitForText('Logons Audit Search', testConfig.TestTimeToWaitForText);
   await I.performLogonAuditSearch();
   await I.click('button[name="logon-search-btn"]');
-  await I.wait(30);
   await I.waitForText('Logons Audit Results', testConfig.TestTimeToWaitForText);
   // Asserting the text after Pagination
   const textBeforePagination = await I.grabTextFromAll('div[class="flex-space-between"] p');
@@ -26,13 +25,11 @@ Scenario('Navigate to LAU, perform logon audit search and authenticate logon sea
   await I.waitForText('Displaying 1 to 100 of 10,000 records', testConfig.TestTimeToWaitForText);
   await I.waitForText('Note: Results returned have been capped at 10,000.', testConfig.TestTimeToWaitForText);
   await I.click('Next >');
-  await I.wait(30);
   const textAfterPagination = await I.grabTextFromAll('div[class="flex-space-between"] p');
   logger.info({message: 'the text is ', textAfterPagination});
   await I.waitForText('Displaying 101 to 200 of 10,000 records', testConfig.TestTimeToWaitForText);
   await I.waitForText('Note: Results returned have been capped at 10,000.', testConfig.TestTimeToWaitForText);
   await I.click('Last >>');
-  await I.wait(30);
   const textLastPagination = await I.grabTextFromAll('div[class="flex-space-between"] p');
   logger.info({message: 'the text is ', textLastPagination});
   await I.waitForText('Displaying 9,901 to 10,000 of 10,000 records', testConfig.TestTimeToWaitForText);
@@ -48,15 +45,12 @@ Scenario('Navigate to LAU, perform logon audit search and download CSV', async (
   await I.amOnPage('/');
   await I.waitForText('Log and Audit', testConfig.TestTimeToWaitForText);
   await lauHelper.selectTab(I, tabs.LOGON_SEARCH);
-  await I.wait(10);
   await I.waitForText('Logons Audit Search', testConfig.TestTimeToWaitForText);
   await I.performLogonAuditSearch();
   await I.click('button[name="logon-search-btn"]');
-  await I.wait(30);
   await I.waitForText('Logons Audit Results', testConfig.TestTimeToWaitForText);
   await I.handleDownloads();
   await I.click('#logonsCsvBtn');
-  await I.wait(10);
 
   const csvPath = lauHelper.getCsvPath();
 
@@ -76,7 +70,6 @@ Scenario('Navigate to LAU, perform logon audit search without search parameters'
   await I.waitForText('Logons Audit Search', testConfig.TestTimeToWaitForText);
   await I.performLogonAuditSearchWithoutSearchData();
   await I.click('button[name="logon-search-btn"]');
-  await I.wait(10);
   await I.waitForText('Please enter at least one of the following fields: User ID or Email', testConfig.TestTimeToWaitForText);
   await I.waitForText('\'Time from\' is required.', testConfig.TestTimeToWaitForText);
   await I.waitForText('\'Time to\' is required.', testConfig.TestTimeToWaitForText);
