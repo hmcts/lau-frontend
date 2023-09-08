9fadd3a11a3b41ae83dc27a6e9cfe78aaf025c0d
Rapolas Kaselis <rapolas.kaselis@hmcts.net>
2023-09-08 09:46:56 +0100

    do not required eslint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
   'parser': '@babel/eslint-parser',
   'parserOptions': {
     'ecmaVersion': 2018,
+    'requireConfigFile': false,
     'sourceType': 'module',
   },
   'rules': {
