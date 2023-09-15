1228bdcd21f1f595fc5d9a37bb8043cdec7beec8
Rapolas Kaselis <rapolas.kaselis@hmcts.net>
2023-09-15 08:02:01 +0100

    ignore routes search in sonar properties

--- src/main/routes/deletedUsers.ts
+++ src/main/routes/deletedUsers.ts
@@ -3,9 +3,7 @@ import {DeletedUsersController} from '../controllers/DeletedUsers.controller';
 
 export default function (app: Application): void {
   const controller = new DeletedUsersController();
-
-  //eslint-disable-next-line
+  
   app.get('/deleted-users/page/:pageNumber', controller.getPage);
-  //eslint-disable-next-line
   app.get('/deleted-users/csv', controller.getCsv);
 }
