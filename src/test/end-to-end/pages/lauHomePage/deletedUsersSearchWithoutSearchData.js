8cbcf75c02651eca0ac30728814bbf3559ab7ac0
Priyanka Verma <priyanka.verma@hmcts.net>
2023-09-15 14:33:32 +0100

    LAU-729 Deleted Userfunctional test

--- src/test/end-to-end/pages/lauHomePage/deletedUsersSearchWithoutSearchData.js
+++ src/test/end-to-end/pages/lauHomePage/deletedUsersSearchWithoutSearchData.js
@@ -1,9 +1,9 @@
 module.exports = async function () {
   const I = this;
-  await I.fillField('User ID', '');
-  await I.fillField('Email', '');
-  await I.fillField('First Name', '');
-  await I.fillField('Last Name', '');
+  await I.fillField('userId', '');
+  await I.fillField('emailAddress', '');
+  await I.fillField('firstName', '');
+  await I.fillField('lastName', '');
   await I.fillField('startTimestamp', '');
   await I.fillField('endTimestamp', '');
 };
