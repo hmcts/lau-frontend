c650e2f3cfd1910142f7f9d196e7d24c98a5aea6
Priyanka Verma <priyanka.verma@hmcts.net>
2023-09-22 16:48:25 +0100

    LAU-742 LaunchDarkley Feature Toggle change

---
+++ src/main/service/FeatureToggleService.ts
@@ -0,0 +1,11 @@
+import config from 'config';
+import { LaunchDarklyClient } from '../components/featureToggle/LaunchDarklyClient';
+
+export class FeatureToggleService{
+    public async getDeletedUsersFeatureToggle(): Promise<void> {
+        const deletedUsersFt = String(config.get('featureToggles.ft_deletedUsers'));
+        const deletedUsersFtValue = await LaunchDarklyClient.instance.variation(deletedUsersFt, false);
+        return deletedUsersFtValue;
+    }
+    
+  }
