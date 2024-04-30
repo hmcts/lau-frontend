4bf48b396838f72bbb906f863de317c018ff9385
sonny-hmcts <131464571+sonny-hmcts@users.noreply.github.com>
2024-04-30 11:42:30 +0100

    global LD mock for all jest tests (#626)
    * global LD mock for all jest tests
    
    * ignore jest.setup from lint
    
    * added jest.setup.js to eslint ignore
    
    ---------
    
    Co-authored-by: Martin Spasov <martin.y.spasov@gmail.com>

--- src/test/unit/components/LaunchDarklyClient.test.ts
+++ src/test/unit/components/LaunchDarklyClient.test.ts
@@ -1,15 +1,26 @@
 import {LaunchDarklyClient} from '../../../main/components/featureToggle/LaunchDarklyClient';
 
 describe('LaunchDarklyClient', () => {
-  it('initialises the client', async () => {
+
+  beforeEach(() => {
+    // Initialize the LaunchDarklyClient with the mock LDClientInterface
     LaunchDarklyClient.initialise();
+  });
+
+  it('initialises the client', async () => {
     expect(LaunchDarklyClient.instance).toBeDefined();
   });
   it('successfully calls and returns truthy variation value', async () => {
+    // Simulate LDClient variation method returning true
+    jest.spyOn(LaunchDarklyClient.instance, 'variation').mockResolvedValue(true);
+
     const testFeature = await LaunchDarklyClient.instance.variation('test', true);
     expect(testFeature).toBeTruthy();
   });
   it('successfully calls and returns falsy variation value', async () => {
+    // Simulate LDClient variation method returning false
+    jest.spyOn(LaunchDarklyClient.instance, 'variation').mockResolvedValue(false);
+
     const testFeature = await LaunchDarklyClient.instance.variation('test', false);
     expect(testFeature).toBeFalsy();
   });
