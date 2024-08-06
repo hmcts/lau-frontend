a0b71b935a8f3938e193600570154afc76ff6ef4
Priyanka Verma <priyanka.verma@hmcts.net>
2024-08-06 10:55:38 +0100

    LAU-927 date-validation

--- src/main/util/Date.ts
+++ src/main/util/Date.ts
@@ -1,4 +1,7 @@
 import moment from 'moment';
+const { Logger } = require('@hmcts/nodejs-logging');
+
+const logger = Logger.getLogger('date');
 
 export const DATE_REGEX = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})$/gm;
 export const DATE_REGEX_WITHOUT_SECONDS = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2})$/gm;
@@ -17,7 +20,11 @@ const SECONDS = ':00';
 export const isDateValid = (date: string): boolean => {
   date = addSeconds(date);
   var currentDate = moment().utc().format(REQUEST_DATE_FORMAT);
-  return date?.match(DATE_REGEX) && moment.utc(date, REQUEST_DATE_FORMAT).isValid() && moment.utc(date, REQUEST_DATE_FORMAT).isSameOrBefore(currentDate);
+  var comingDate = moment(date).utc().format(REQUEST_DATE_FORMAT);
+
+  logger.info(`Current Date : ${currentDate}`);
+  logger.info(`Coming Date : ${comingDate}`);
+  return date?.match(DATE_REGEX) && moment.utc(date, REQUEST_DATE_FORMAT).isValid() && moment(comingDate).isSameOrBefore(currentDate);
 };
 
 export const formDateToRequestDate = (date: string): string => {
