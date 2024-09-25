95985c00b0d7e364af562c70c7a7558f18173f61
Priyanka Verma <priyanka.verma@hmcts.net>
2024-09-25 16:13:52 +0100

    LAU-959 adding test

--- src/test/unit/components/LaunchDarklyClient.test.ts
+++ src/test/unit/components/LaunchDarklyClient.test.ts
@@ -1,4 +1,8 @@
 import {LaunchDarklyClient} from '../../../main/components/featureToggle/LaunchDarklyClient';
+import { FeatureToggleService } from '../../../main/service/FeatureToggleService';
+import config from 'config';
+
+jest.mock('config');
 
 describe('LaunchDarklyClient', () => {
 
@@ -23,5 +27,30 @@ describe('LaunchDarklyClient', () => {
 
     const testFeature = await LaunchDarklyClient.instance.variation('test', false);
     expect(testFeature).toBeFalsy();
+  });
+  it('successfully calls and returns true variation value when enviornment is development', async () => {
+    (config.get as jest.Mock).mockImplementation((key: string) => {
+      if (key === 'is_dev') return 'true';
+      if (key === 'featureToggles.ft_challengeAccess') return 'ft_challengeAccess';
+      return null;
+    });
+    // Simulate LDClient variation method returning true when development environment
+    jest.spyOn(LaunchDarklyClient.instance, 'variation').mockResolvedValue(true);
+
+    const result = await FeatureToggleService.getChallengedAccessFeatureToggle();
+    expect(result).toBeTruthy();
+  });
+  it('successfully calls and returns false variation value when enviornment is production', async () => {
+    (config.get as jest.Mock).mockImplementation((key: string) => {
+      if (key === 'is_dev') return 'false';
+      if (key === 'featureToggles.ft_challengeAccess') return 'ft_challengeAccess';
+      return null;
+    });
+    // Simulate LDClient variation method returning true when development environment
+    jest.spyOn(LaunchDarklyClient.instance, 'variation').mockResolvedValue(false);
+
+    const result = await FeatureToggleService.getChallengedAccessFeatureToggle();
+    expect(result).toBeFalsy();
+  
   });
 });
