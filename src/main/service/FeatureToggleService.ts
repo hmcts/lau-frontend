8c2922844cb9d034d6460783f31c360002fe9bff
priyankaVerma21 <priyanka.verma@HMCTS.NET>
2024-09-27 11:10:14 +0100

    LAU-959 enabling challended and specific tab on development (#786)
    * LAU-959 enabling challended and specific tab on development
    
    * LAU-959 adding test
    
    * LAU-959 adding test
    
    * LAU-959 adding test
    
    * LAU-959 adding test

--- src/main/service/FeatureToggleService.ts
+++ src/main/service/FeatureToggleService.ts
@@ -9,7 +9,8 @@ export class FeatureToggleService{
   }
 
   public static async getChallengedAccessFeatureToggle(): Promise<void> {
+    const isDevelopment = Boolean(config.get('is_dev'));
     const challengedAccessFt = String(config.get('featureToggles.ft_challengeAccess'));
-    return await LaunchDarklyClient.instance.variation(challengedAccessFt, false);
+    return await LaunchDarklyClient.instance.variation(challengedAccessFt, isDevelopment);
   }
 }
