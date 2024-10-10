abc797a4953c347c30e56e2db76ac509dfd3dd80
Martin <martin.spasov@hmcts.net>
2024-10-10 09:53:00 +0100

    LAU-976 Remove start timestamp

--- src/main/models/challenged-access/CaseChallengedAccessLogs.ts
+++ src/main/models/challenged-access/CaseChallengedAccessLogs.ts
@@ -6,23 +6,21 @@ export interface CaseChallengedAccessLog {
   caseRef: string;
   userId: string;
   action: string;
-  requestStart: string;
   requestEnd: string;
   reason: string;
   timestamp: string;
 }
 
-export const caseChallengedAccessLogOrder = ['caseRef', 'userId', 'requestType', 'action', 'reason', 'requestStartTimestamp', 'requestEndTimestamp', 'timestamp'];
+export const caseChallengedAccessLogOrder = ['userId', 'caseRef', 'requestType', 'action', 'timestamp','reason','requestEndTimestamp'];
 
 export const textMapping: { [key: string]: string } = {
-  caseRef: 'Case Ref',
   userId: 'User ID',
-  requestType: 'Type',
-  action: 'Operation',
-  reason: 'Explanation',
-  requestStartTimestamp: 'Request Start',
-  requestEndTimestamp: 'Request End',
-  timestamp: 'Timestamp (UTC)',
+  caseRef: 'Case ID',
+  requestType: 'Request Type',
+  action: 'Action',
+  timestamp: 'Action On (UTC)',
+  reason: 'Justification',
+  requestEndTimestamp: 'Time Limit (UTC)',
 };
 
 export function getCaseChallengedAccessLogOrder(): { text: string }[]  {
