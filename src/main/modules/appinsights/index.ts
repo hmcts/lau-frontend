81eb04d165063fdc865dd0b8b86923ca9475306e
Rapolas <130578328+rapolaskaseliscgi@users.noreply.github.com>
2025-03-04 10:34:26 +0000

    hardcode service name

--- src/main/modules/appinsights/index.ts
+++ src/main/modules/appinsights/index.ts
@@ -5,33 +5,16 @@ import config from 'config';
 export class AppInsights {
 
   enable(): void {
-    const connString: string = config.get('appInsights.connectionString');
-    this.logConnString(connString);
-    if (connString) {
+    if (config.get('appInsights.connectionString')) {
       appInsights.setup(config.get<string>('appInsights.connectionString'))
         .setSendLiveMetrics(true)
         .setAutoCollectConsole(true, true)
         .setAutoCollectExceptions(true)
         .start();
 
-      const serviceName = config.get('service.name');
       appInsights.defaultClient.config.samplingPercentage = config.get('appInsights.samplingPercentage');
-      appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = serviceName;
+      appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = 'lau-frontend';
       appInsights.defaultClient.trackTrace({message: 'App insights activated'});
-    }
-  }
-
-  logConnString(connString: string): void {
-    if (connString) {
-      const parts = connString.split(';');
-      const keyValues = parts
-        .map((part: string) => part.split('='))
-        .reduce((res: {[key:string]: string}, pair) => {
-          res[pair[0]] = pair[1];
-          return res;
-        }, {});
-      console.warn('logdebug application id:', keyValues['ApplicationId']);
-      console.warn('logdebug ingestion point:', keyValues['IngestionEndpoint']);
     }
   }
 }
