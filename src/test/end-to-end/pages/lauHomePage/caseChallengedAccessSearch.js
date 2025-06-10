bdc5cc926a692cf039c67768daefed2f2e85c7fa
Martin Spasov <martin.y.spasov@gmail.com>
2025-06-10 09:23:46 +0100

    Update error message link to point to first available search field (#981)
    Co-authored-by: Martin <martin.spasov@hmcts.net>

--- src/test/end-to-end/pages/lauHomePage/caseChallengedAccessSearch.js
+++ src/test/end-to-end/pages/lauHomePage/caseChallengedAccessSearch.js
@@ -1,7 +1,7 @@
 module.exports = async function () {
   const I = this;
   await I.fillField('#caseRef', '0123456789012345');
-  await I.fillField('#caseUserId', '00000000-1111-2222-3333-445566778899');
+  await I.fillField('#first-search-field', '00000000-1111-2222-3333-445566778899');
   await I.selectOption('#requesttype', 'CHALLENGED');
   await I.fillStartTime('2022-05-30T14:42:00');
   await I.fillEndTime('2022-06-02T14:42:00');
