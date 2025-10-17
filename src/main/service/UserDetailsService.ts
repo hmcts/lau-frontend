95ff3fc03c250536e2a5a282775ca1f423fd0b67
Rapolas <130578328+rapolaskaseliscgi@users.noreply.github.com>
2025-10-17 14:28:40 +0100

    LAU-1223 log full api call

--- src/main/service/UserDetailsService.ts
+++ src/main/service/UserDetailsService.ts
@@ -14,8 +14,9 @@ export class UserDetailsService extends BaseService<UserDetailsSearchRequest> {
   public getUserDetails(req: AppRequest<UserDetailsSearchRequest>, isEmail: boolean): Promise<UserDetailsAuditData> {
     const endpoint: string = config.get('services.lau-eud-backend.endpoints.userDetails');
     const userIdOrEmail = req.session.userDetailsFormState.userIdOrEmail;
-    logger.info(`Calling userDetails for user ${userIdOrEmail}`);
     const queryParam = isEmail? 'email': 'userId';
-    return this.get(req.session, `${endpoint}?${queryParam}=${userIdOrEmail}`) as Promise<UserDetailsAuditData>;
+    const path = `${endpoint}?${queryParam}=${userIdOrEmail}`;
+    logger.info(`Calling userDetails - ${this.baseApiUrl}${path}`);
+    return this.get(req.session, path) as Promise<UserDetailsAuditData>;
   }
 }
