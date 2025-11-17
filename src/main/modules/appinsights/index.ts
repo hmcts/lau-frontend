0aefb09d8160e74cd0761b47b9180c30c45d320d
Rapolas <130578328+rapolaskaseliscgi@users.noreply.github.com>
2025-11-17 17:20:37 +0000

    LAU-1319 set cloud role name for lau-frontend

--- src/main/modules/appinsights/index.ts
+++ src/main/modules/appinsights/index.ts
@@ -10,11 +10,9 @@ export class AppInsights {
         .setSendLiveMetrics(true)
         .setAutoCollectConsole(true, true)
         .setAutoCollectExceptions(true)
-        .setAutoCollectDependencies(false) // Disable dependency tracking
-        .setAutoDependencyCorrelation(false); //  Disable automatic dependency correlation
+        .setAutoCollectDependencies(false); // Disable dependency tracking
 
       appInsights.defaultClient.config.samplingPercentage = config.get('appInsights.samplingPercentage');
-      appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = 'lau-frontend';
       appInsights.defaultClient.trackTrace({message: 'App insights activated'});
       appInsights.start();
     }
