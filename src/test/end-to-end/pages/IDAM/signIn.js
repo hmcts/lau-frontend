d27fd94fafb72e7172790a3c064b2a338fd24707
Priyanka Verma <priyanka.verma@hmcts.net>
2026-03-18 12:28:27 +0000

    LAU-1412 Idam sign in changes

--- src/test/end-to-end/pages/IDAM/signIn.js
+++ src/test/end-to-end/pages/IDAM/signIn.js
@@ -9,11 +9,36 @@ module.exports = async function (givenUserType, isAlreadyAtSignOnPage = false) {
   if (!isAlreadyAtSignOnPage) {
     await I.amOnLoadedPage('/');
   }
+  let doClassicLogin = true;
+  let doModernLogin = true;
+  try 
+  {
+      await I.waitForText('Sign in', testConfig.TestTimeToWaitForText);
+      await I.fillField('#username', user.email);
+      await I.fillField('#password', user.password);
+      await I.waitForNavigationToComplete('input[type="submit"]');
+  } catch (e){
+    doClassicLogin = false;
+  }
 
-  await I.waitForText('Sign in', testConfig.TestTimeToWaitForText);
+  if (doClassicLogin) {
+    return;
+  }
 
-  await I.fillField('#username', user.email);
-  await I.fillField('#password', user.password);
+  try{
+    await I.waitForText('Enter your email address', testConfig.TestTimeToWaitForText);
+    await I.fillField('#email', user.email);
+    await I.click('Continue');
 
-  await I.waitForNavigationToComplete('input[type="submit"]');
+    await I.waitForText('Enter your password', testConfig.TestTimeToWaitForText);
+    await I.fillField('#password', user.password);
+    await I.click('Continue');
+  } catch (e){
+    doModernLogin = false;
+  }
+
+  if (doModernLogin) {
+    return;
+  }
+
 };
