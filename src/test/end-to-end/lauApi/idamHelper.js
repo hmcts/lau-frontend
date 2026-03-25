8482bbb9d674dd31b4382d9e1311212fa5927a9f
Priyanka Verma <priyanka.verma@hmcts.net>
2026-03-25 14:15:02 +0000

    LAU-1414 date check

--- src/test/end-to-end/lauApi/idamHelper.js
+++ src/test/end-to-end/lauApi/idamHelper.js
@@ -1,3 +1,4 @@
+const { update } = require('lodash');
 const config = require('../../config.js');
 const restHelper = require('./restHelper');
 
@@ -8,7 +9,9 @@ module.exports = {
   accessToken: async (user) => {
     return restHelper.retriedRequest(
       `${idamUrl}/${loginEndpoint}?username=${encodeURIComponent(user.email)}&password=${user.password}`,
-      {'Content-Type': 'application/x-www-form-urlencoded'})
+      {'Content-Type': 'application/x-www-form-urlencoded'},
+      null,
+      'POST')
       .then(response => response.json())
       .then(data => data.access_token);
   },
@@ -19,7 +22,9 @@ module.exports = {
       {
         'Content-Type': 'application/x-www-form-urlencoded',
         'Authorization': `Bearer ${authToken}`,
-      })
+      },
+      null,
+      'POST')
       .then(response => response.json())
       .then(data => data.uid);
   },
@@ -30,4 +35,26 @@ module.exports = {
       {}, null, 'GET')
       .then(response => response.text());
   },
-};
+
+  clientCredentialsAccessToken: async(idamClientSecret,scope) => {
+    return restHelper.retriedRequest(
+      `${idamUrl}/o/token`,
+      {'Content-Type': 'application/x-www-form-urlencoded'},
+      `grant_type=client_credentials&client_id=lau&client_secret=${idamClientSecret}&scope=${scope}`,
+      'POST')
+      .then(response => response.json())
+      .then(data => data.access_token);
+  },
+
+  updateUserDetails: async (accessToken, userId, userEmail) => {
+    return restHelper.retriedRequest(
+      `${idamUrl}/api/v2/users/${userId}`,
+      {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`,
+      },
+      JSON.stringify({ email: userEmail, forename: "TestForename", surname: "TestSurname"}),
+      'PUT'
+    );
+  },
+}; 
