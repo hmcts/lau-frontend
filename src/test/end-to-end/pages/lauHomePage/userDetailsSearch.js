736c732d8b663e326d2a997eb09789371906cd8f
Priyanka Verma <priyanka.verma@hmcts.net>
2026-03-26 10:43:28 +0000

    UserId changed

--- src/test/end-to-end/pages/lauHomePage/userDetailsSearch.js
+++ src/test/end-to-end/pages/lauHomePage/userDetailsSearch.js
@@ -1,4 +1,4 @@
 module.exports = async function () {
   const I = this;
-  await I.fillField('#userIdOrEmail', '072921f8-328e-4e3b-8c9d-918f769bd7a7');
+  await I.fillField('#userIdOrEmail', 'f7d13b63-efdc-482b-8238-df56abe3c169');
 };
