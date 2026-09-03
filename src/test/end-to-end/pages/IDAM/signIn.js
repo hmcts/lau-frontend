42eedeff9915c1894a45d70c3c0b73b02a0952bd
Rapolas <130578328+rapolaskaseliscgi@users.noreply.github.com>
2026-09-03 12:41:01 +0100

    debug

--- src/test/end-to-end/pages/IDAM/signIn.js
+++ src/test/end-to-end/pages/IDAM/signIn.js
@@ -11,27 +11,11 @@ module.exports = async function (givenUserType, isAlreadyAtSignOnPage = false) {
     await I.amOnLoadedPage('/');
   }
 
-  const didClassicLoginWork = await tryTo(async () => {
-    I.see('Sign in', 'h1');
-    I.fillField('#username', user.email);
-    I.fillField('#password', user.password);
-    I.click('Sign in');
-  
-  });
+  I.see('Enter your email address', 'h1');
+  I.fillField('#email', user.email);
+  I.click('Continue');
 
-  if (!didClassicLoginWork) {
-    const didModernLoginWork = await tryTo(async () => {
-      I.see('Enter your email address', 'h1');
-      I.fillField('#email', user.email);
-      I.click('Continue');
-
-      I.see('Enter your password', 'h1');
-      I.fillField('#password', user.password);
-      I.click('Continue');
-    });
-
-    if (!didModernLoginWork) {
-      throw new Error('Classic and modern login both failed.');
-    }
-  };
+  I.see('Enter your password', 'h1');
+  I.fillField('#password', user.password);
+  I.click('Continue');
 };
